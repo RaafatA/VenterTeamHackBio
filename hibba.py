971c3ff8398a1ef21c0d9d1cc6f1614aa92389dc
Hibba0 <88403820+Hibba0@users.noreply.github.com>
2021-08-06 05:15:09 -0700

    Add files via upload

---
+++ hibba.py
@@ -0,0 +1,26 @@
+def hammingDist(str1, str2):
+    i = 0
+    count = 0
+ 
+    while(i < len(str1)):
+        if(str1[i] != str2[i]):
+            count += 1
+        i += 1
+    return count
+ 
+# Driver code 
+str1 = "hibba0"
+str2 = "hibba"
+ 
+# function call
+print(hammingDist(str1, str2))
+
+
+
+
+ def personal_details():
+    name, slack_username = "HIBBA", "2hibba"
+    email , twitter_handle= "hibba.bsbi588@iiu.edu.pk", "@hibba0
+    print("Name: {}\n: slack_username{}\n:  email{}\n: twitter_handle {}".format(HIBBA,hibba.bsbi588@iiu.edu.pk ,@hibba,@hibba0 ))
+
+personal_details()
