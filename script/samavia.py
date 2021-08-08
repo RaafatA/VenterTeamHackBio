94226582dc9bbcb30f3ff60c447b21c76d19baec
Raafat Abdulmajeed Fahmy <76243557+RaafatA@users.noreply.github.com>
2021-08-08 19:28:23 +0200

    Update samavia.py

--- script/samavia.py
+++ script/samavia.py
@@ -1,7 +1,7 @@
-myname="Samavia urooj"
+myname="Samavia_urooj"
 myemail="sammavia.bsbi574@gmail.com"
 myusername="Samavia"
-mybiostack="Drug development and Genomics"
+mybiostack="Drug_development_and_Genomics"
 mytwitter="@samavia"
 hammingdistance=2
 print(myname,myemail,myusername,mybiostack,mytwitter,hammingdistance, sep=',')
