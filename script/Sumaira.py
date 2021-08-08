158baf020cbc6e61d66501258cd69452cbbcf9f8
Raafat Abdulmajeed Fahmy <76243557+RaafatA@users.noreply.github.com>
2021-08-08 19:26:57 +0200

    Update Sumaira.py

--- script/Sumaira.py
+++ script/Sumaira.py
@@ -1,7 +1,7 @@
-myname="Sumaira Fatima"
+myname="Sumaira_Fatima"
 myemail="sumaira.bsbi598@gmail.com"
 myusername="Sumaira"
-mybiostack="Drug development and Genomics"
+mybiostack="Drug_development_and_Genomics"
 mytwitter="@sumaira"
 hammingdistance=1
 print(myname,myemail,myusername,mybiostack,mytwitter,hammingdistance, sep=',')
