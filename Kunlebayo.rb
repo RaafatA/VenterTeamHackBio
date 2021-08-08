e572f55b1ac6f421a2ac1c13bc08033afbf181c7
Samuel Oluwafemi Egbuwalo <femiegbuwalo@gmail.com>
2021-08-08 18:07:55 +0200

    Add files via upload

---
+++ Kunlebayo.rb
@@ -0,0 +1,9 @@
+name = "Adebayo Oluwafemi Adekunle"
+email = "adekunleadebayoolufemi@gmail.com"
+slackusername = "@Kunlebayo"
+biostack = "genomics"
+twitterhandle = "@Kunlebayo"
+
+hammingdistance = "0"
+
+print name + "," + email + "," + slackusername + "," + biostack + "," + twitterhandle + "," + hammingdistance
