4d6623b1b8e994a3e79b2c4e4aff8d3e7411cf53
Samuel Oluwafemi Egbuwalo <femiegbuwalo@gmail.com>
2021-08-08 18:14:16 +0200

    Add files via upload

---
+++ ureeza.java
@@ -0,0 +1,27 @@
+package ureeza.java;
+public class Ureeza {
+    static int hammingDistance(String str1, String str2)
+{
+    int i = 0, count = 0;
+    while (i < str1.length())
+    {
+        if (str1.charAt(i) != str2.charAt(i))
+            count++;
+        i++;
+    }
+    return count;
+}
+    public static void main(String[] args) {
+        
+        String str1 = "Ureeza";
+    String str2 = "Yurrii";
+   int mp=hammingDistance (str1, str2);
+        
+ 
+    System.out.print("@Yurrii,@Ureeza,bioinformatics,Ureeza Waqar,ureezawaqar0@gmail.com,");
+     System.out.print(mp);
+
+
+    }
+    
+}
