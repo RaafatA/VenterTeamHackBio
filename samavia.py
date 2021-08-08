7a32a89510a1bb7268e65c4742862a2f3f3bfb05
samavia096 <88532507+samavia096@users.noreply.github.com>
2021-08-08 09:18:42 -0700

    Update samavia.py

--- samavia.py
+++ samavia.py
@@ -1,31 +1,7 @@
-#Skip to content
-#smavia0 / test - repo
-#Unwatch 1
-
-
-def hammingDist(str1, str2):
-    i = 0
-    count = 0
-
-    while (i < len(str1)):
-        if (str1[i] != str2[i]):
-            count += 1
-        i += 1
-    return count
-
-
-# Driver code
-str1 = "smavia096"
-str2 = "smavia"
-
-# function call
-print(hammingDist(str1, str2))
-
-
-def personal_details():
-    name, slack_username = "SMAVIA", "@smavia"
-    email, twitter_handle = "samavia2345@gmail.com", "@smavia096"
-    print("Name: {}\n: slack_username{}\n:  email{}\n: twitter_handle {}".format("SMAVIA",
-                                                                                 "samavia2345 @ gmail.com", "@ smavia", "@ smavia096" ))
-
-    personal_details()
+myname="Samavia urooj"
+myemail="sammavia.bsbi574@gmail.com"
+myusername="Samavia"
+mybiostack="Drug development and Genomics"
+mytwitter="@samavia"
+hammingdistance=2
+print(myname,myemail,myusername,mybiostack,mytwitter,hammingdistance, sep=',')
